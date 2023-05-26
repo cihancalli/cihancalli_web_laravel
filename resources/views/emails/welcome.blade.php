f705b4dcb2944d94bfdd4115185a9da602dc29f2
Cihan Çallı <cihan.callii@gmail.com>
2023-05-26 18:27:20 +0300

    first commit

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <title>Hoş geldiniz!</title>
+</head>
+<body>
+<h1>Hoş geldiniz!</h1>
+<p>Merhaba,</p>
+<p>Hoş geldiniz! Uygulamamıza katıldığınız için teşekkür ederiz.</p>
+</body>
+</html>
